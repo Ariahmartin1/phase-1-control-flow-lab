406f1c9751cbcf56341314fe26c7d95b29789c59
zachariah martin <zachariahsmartin@gmail.com>
2022-09-03 11:17:23 -0400

    commit tree 2cfd62439c486be889cbe71ade464acbc621341d

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetTraveled){
+  let price
+  if (feetTraveled <= 400) {
+    price = 'This one is on me!'
+  }
+  else if (feetTraveled > 400 && feetTraveled <= 2000) {
+    price = 'That will be twenty bucks.'
+  }
+  else if (feetTraveled > 2000 && feetTraveled <= 2500)
+  price = 'I will gladly take your thirty bucks.'
+  else {
+    return 'No can do.'
+  }
+  return price
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ switch ( tip){
+    case "generous" :
+      return "Thank you so much."
+    case "not as generous" :
+      return "Thank you."
+    default :
+      return "Bye."
+ }
 }
